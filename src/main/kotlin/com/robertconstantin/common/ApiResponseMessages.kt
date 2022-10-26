eab69d56a6407856bebf07aafcbe8e0282193582
ROBERT1830 <robert@beatingbrain.com>
2022-10-26 19:38:35 +0200

    rEFACTORED AUTH ROUTES AND CHANGED KMONGO VERSION

--- src/main/kotlin/com/robertconstantin/common/ApiResponseMessages.kt
+++ src/main/kotlin/com/robertconstantin/common/ApiResponseMessages.kt
@@ -4,6 +4,7 @@ object ApiResponseMessages {
     //User authentication
     const val USER_ALREADY_EXISTS = "User already exists!"
     const val FIELDS_BLANK = "Some fields are empty!"
+    const val SOMETHING_WENT_WRONG = "Oops, something went wrong, please try again!"
     const val INVALID_CREDENTIALS = "Oops, that is not correct, please try again!"
     const val USER_NOT_SUBSCRIBED = "Oops, the subscription couldn't be performed!"
     const val USER_CREDENTIALS_NO_UPDATE = "Oops, we couldn't update your credentials, try again!"
---
+++ src/main/kotlin/com/robertconstantin/routes/util/Constants.kt
@@ -0,0 +1,5 @@
+package com.robertconstantin.routes.util
+
+object Constants {
+    const val PROFILE_DATA = "profile_data"
+}
